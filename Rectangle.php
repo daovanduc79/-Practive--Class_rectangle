ed12063172ece3c95d13d63abb55d7f493cf444c
daovanduc <duc07092000@gmail.com>
2020-04-16 22:56:17 +0700

    update class Rectangle

---
+++ Rectangle.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Rectangle
+{
+    public $width;
+    public $height;
+
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function getArea()
+    {
+        return $this->height * $this->width;
+    }
+
+    public function getPerimeter()
+    {
+        return ($this->height + $this->width) * 2;
+    }
+
+    public function display()
+    {
+        return "Rectangle{" . "width=" . $this->width . ", height=" . $this->height . "}";
+    }
+
+}
